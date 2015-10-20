2809d60b39705c5b4ec566ca88d88a83cf151160
MatthewLaBianca <Matthew.Labianca@IGT.com>
2015-10-20 10:01:17 -0400

    game manager

---
+++ IGTMobile/Assets/GameManagerStuff/CPWinUpdate.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CPWinUpdate : MonoBehaviour {
+
+    public Manager gameManager;
+    public CPEndScreen endScreen;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void UpdateBalance()
+    {
+        gameManager.ChangeBalanceBy(endScreen.GetWinAmount());
+        Debug.Log("Added " + endScreen.GetWinAmount());
+    }
+}
---
+++ IGTMobile/Assets/Scripts/CPEndScreen.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CPEndScreen : MonoBehaviour {
+
+    public BalloonTouch matchesScript;
+    private int matches;
+    private int winAmnt;
+    public Text winTxt;
+    public Manager gameManager;
+
+	// Use this for initialization
+	void Start () {
+        matches = matchesScript.matches;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        SetWinAmount(matches);
+        winTxt.text = "$" + winAmnt.ToString();
+	}
+
+    void SetWinAmount(int amntMatched)
+    {
+        switch (amntMatched)
+        {
+            case 0:
+                winAmnt = 0;
+                break;
+            case 1:
+                winAmnt = 0;
+                break;
+            case 2:
+                winAmnt = 1;
+                break;
+            case 3:
+                winAmnt = 10;
+                break;
+            case 4:
+                winAmnt = 100;
+                break;
+            case 5:
+                winAmnt = 500;
+                break;
+        }
+    }
+
+    public int GetWinAmount()
+    {
+        
+        return winAmnt;
+    }
+}
